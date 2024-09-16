192923c436a5ca6552fa0ea191ac97baa820401b
Lars Frölich <lars.froelich@gmail.com>
2024-09-16 22:53:38 +0200

    gateway: fixed self-provisioning routine SSL connectivity

--- gateway/src/self_provisioning.py
+++ gateway/src/self_provisioning.py
@@ -1,6 +1,9 @@
+import json
 import os
 import socket
+import ssl
 from random import randrange
+from time import sleep
 
 from paho.mqtt.client import Client
 
@@ -17,19 +20,32 @@ def self_provisioning_get_access_token(args):
     # else, perform self-provisioning
     print("No access token found, performing self-provisioning...")
     mqtt_client = Client()
-    mqtt_client.username_pw_set("provision", "")
+    mqtt_client.username_pw_set("provision", None)
+    mqtt_client.tls_set(cert_reqs=ssl.CERT_REQUIRED)
+    mqtt_client.on_connect = (lambda client, userdata, flags, rc: print(f"Connected to ThingsBoard for self-provisioning with result code {rc}"))
     mqtt_client.connect(args.tb_host, args.tb_port)
-    print("Connected to ThingsBoard for self-provisioning")
-    mqtt_client.subscribe("/provision/response")
-    mqtt_client.publish("/provision/request", "self-provisioning")
+    mqtt_client.on_message = (lambda client, userdata, message: print(f"Received message: {message.payload}"))
+    mqtt_client.subscribe("/provision/response", qos=1)
+    mqtt_client.loop_start()
+    sleep(0.1)
+    mqtt_client.publish("/provision/request",
+                        payload=json.dumps({
+                            "deviceName": get_device_name(args),
+                            "provisionDeviceKey": "u89nftek43npopnvnt21",
+                            "provisionDeviceSecret": "r4acqug0fzh2wii4o4n6"
+                        }), qos=1 )
+
+    for _ in range(100):
+        sleep(0.1)
+    mqtt_client.loop_stop()
     mqtt_client.disconnect()
 
-    return access_token
+    return "x"#access_token
 
 
 def get_device_name(args):
     return (
-            args.device_name
+            getattr(args, "device_name", None)
         or os.environ.get("THINGSBOARD_DEVICE_NAME")
         or socket.gethostname()
         or ("acropolis-gateway-" + str(randrange(1000000, 9999999, 1)))
