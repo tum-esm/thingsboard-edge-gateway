6676ef1548f8be3ec900308c1294d47bad98cb2d
Patrick <patrick.aigner@tum.de>
2024-10-07 22:18:50 +0200

    remove configuration procedure import

--- software/src/procedures/__init__.py
+++ software/src/procedures/__init__.py
@@ -1,4 +1,3 @@
 from .calibration import CalibrationProcedure
-from .configuration import ConfigurationProcedure
 from .measurement import WindMeasurementProcedure, CO2MeasurementProcedure
 from .system_check import SystemCheckProcedure
