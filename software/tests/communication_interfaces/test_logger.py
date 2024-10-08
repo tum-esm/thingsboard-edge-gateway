d57ee52b681e4f3f1e37b9281a8df13e2f17d3cd
Patrick <patrick.aigner@tum.de>
2024-10-08 08:04:11 +0200

    remove MQTT client lines from tests

--- software/tests/communication_interfaces/test_logger.py
+++ software/tests/communication_interfaces/test_logger.py
@@ -3,7 +3,7 @@ import json
 import os
 import time
 import pytest
-from ..pytest_utils import expect_log_file_contents, wait_for_condition
+from ..pytest_utils import expect_log_file_contents
 from os.path import dirname, abspath, join
 import sys
 
@@ -27,7 +27,8 @@ def test_logger_with_sending(messaging_agent_with_sending: None) -> None:
 
 @pytest.mark.remote_update
 @pytest.mark.github_action
-def test_very_long_exception_cutting(messaging_agent_with_sending: None) -> None:
+def test_very_long_exception_cutting(
+        messaging_agent_with_sending: None) -> None:
     config = utils.ConfigInterface.read()
     config.active_components.send_messages_over_mqtt = True
     message_queue = utils.MessageQueue()
@@ -35,39 +36,25 @@ def test_very_long_exception_cutting(messaging_agent_with_sending: None) -> None
     logger = utils.Logger(origin="pytests")
 
     message = utils.get_random_string(length=300)
-    details = "\n".join(
-        [utils.get_random_string(length=80) for i in range(250)]
-    )  # 20.249 characters long
+    details = "\n".join([
+        utils.get_random_string(length=80) for i in range(250)
+    ])  # 20.249 characters long
 
     expected_log_file_content = (
-        f"pytests                 - ERROR         - {message}\n"
-        + "--- details: -----------------\n"
-        + f"{details}\n"
-        + "------------------------------\n"
-    )
+        f"pytests                 - ERROR         - {message}\n" +
+        "--- details: -----------------\n" + f"{details}\n" +
+        "------------------------------\n")
     expected_mqtt_message = (
-        f"pytests - {message[: (256 - 31)]} ... CUT (310 -> 256)"
-        + " "
-        + f"{details[: (16384 - 25)]} ... CUT (20249 -> 16384)"
-    )
+        f"pytests - {message[: (256 - 31)]} ... CUT (310 -> 256)" + " " +
+        f"{details[: (16384 - 25)]} ... CUT (20249 -> 16384)")
 
-    assert len(message_queue.get_rows_by_status("pending")) == 0
-    expect_log_file_contents(forbidden_content_blocks=[expected_log_file_content])
+    expect_log_file_contents(
+        forbidden_content_blocks=[expected_log_file_content])
 
     logger.error(message=message, config=config, details=details)
 
-    mqtt_messages = message_queue.get_rows_by_status("pending")
-    assert len(mqtt_messages) == 1
-    mqtt_message_content = mqtt_messages[0].content
-    assert isinstance(mqtt_message_content, custom_types.MQTTLogMessage)
-    assert mqtt_message_content.body.message == expected_mqtt_message
-    expect_log_file_contents(required_content_blocks=[expected_log_file_content])
-
-    wait_for_condition(
-        lambda: len(message_queue.get_rows_by_status("pending")) == 0,
-        timeout_message="message was not sent",
-        timeout_seconds=10,
-    )
+    expect_log_file_contents(
+        required_content_blocks=[expected_log_file_content])
 
 
 def _test_logger(mqtt_communication_enabled: bool) -> None:
@@ -86,32 +73,6 @@ def _test_logger(mqtt_communication_enabled: bool) -> None:
     ]
 
     expect_log_file_contents(forbidden_content_blocks=generated_log_lines)
-    assert len(
-        [
-            m
-            for m in (
-                message_queue.get_rows_by_status("in-progress")
-                + message_queue.get_rows_by_status("pending")
-            )
-        ]
-    ) == (1 if mqtt_communication_enabled else 0)
-
-    TEST_MESSAGE_DATE_STRING = datetime.now().strftime("%Y-%m-%d")
-    MESSAGE_ARCHIVE_FILE = join(
-        PROJECT_DIR,
-        "data",
-        "archive",
-        f"mqtt-messages-{TEST_MESSAGE_DATE_STRING}.json",
-    )
-    EXPECTED_MQTT_TOPIC = (
-        (
-            os.environ["THINGSBOARD_MQTT_BASE_TOPIC"]
-            + "log-messages/"
-            + os.environ["THINGSBOARD_MQTT_IDENTIFIER"]
-        )
-        if mqtt_communication_enabled
-        else None
-    )
 
     # -------------------------------------------------------------------------
     # check whether logs lines were written correctly
@@ -128,70 +89,3 @@ def _test_logger(mqtt_communication_enabled: bool) -> None:
         logger.exception(e, label="customlabel", config=config)
 
     expect_log_file_contents(required_content_blocks=generated_log_lines)
-
-    # -------------------------------------------------------------------------
-    # check whether all records are correctly inserted in sending queue
-
-    active_logs_messages = [
-        custom_types.MQTTLogMessage(**m.content.dict())
-        for m in (
-            message_queue.get_rows_by_status("pending")
-            if mqtt_communication_enabled
-            else []
-        )
-    ]
-    active_logs_messages.sort(key=lambda m: m.body.timestamp)
-    assert len(active_logs_messages) == (4 if mqtt_communication_enabled else 0)
-
-    if mqtt_communication_enabled:
-        assert active_logs_messages[0].header.mqtt_topic == None
-        assert active_logs_messages[0].body.severity == "warning"
-        assert active_logs_messages[0].body.message == "pytests - some message c "
-
-        assert active_logs_messages[1].header.mqtt_topic == None
-        assert active_logs_messages[1].body.severity == "error"
-        assert active_logs_messages[1].body.message == "pytests - some message d "
-
-        assert active_logs_messages[2].header.mqtt_topic == None
-        assert active_logs_messages[2].body.severity == "error"
-
-        # -------------------------------------------------------------------------
-        # wait until sending queue is empty
-
-        def empty_pending_queue() -> bool:
-            return (
-                len(message_queue.get_rows_by_status("pending"))
-                + len(message_queue.get_rows_by_status("in-progress"))
-            ) == 0
-
-        wait_for_condition(
-            empty_pending_queue,
-            timeout_seconds=12,
-            timeout_message="active queue is not empty after 12 second timeout",
-        )
-
-        time.sleep(2)
-
-    # -------------------------------------------------------------------------
-    # check whether archive contains correct messages
-
-    archived_log_messages = []
-
-    with open(MESSAGE_ARCHIVE_FILE, "r") as f:
-        for message in [json.loads(m) for m in f.read().split("\n") if len(m) > 0]:
-            try:
-                archived_log_messages.append(custom_types.MQTTLogMessage(**message))
-            except:
-                pass
-    archived_log_messages.sort(key=lambda m: m.body.timestamp)
-    assert len(archived_log_messages) == 4
-    assert archived_log_messages[0].header.mqtt_topic == None
-    assert archived_log_messages[0].body.severity == "warning"
-    assert archived_log_messages[0].body.message == "pytests - some message c "
-
-    assert archived_log_messages[1].header.mqtt_topic == None
-    assert archived_log_messages[1].body.severity == "error"
-    assert archived_log_messages[1].body.message == "pytests - some message d "
-
-    assert archived_log_messages[2].header.mqtt_topic == None
-    assert archived_log_messages[2].body.severity == "error"
--- software/tests/conftest.py
+++ software/tests/conftest.py
@@ -12,9 +12,8 @@ from src import custom_types, utils, procedures
 PROJECT_DIR = dirname(dirname(abspath(__file__)))
 
 
-def _save_file(
-    original_path: str, temporary_path: str, test_content: Optional[str]
-) -> None:
+def _save_file(original_path: str, temporary_path: str,
+               test_content: Optional[str]) -> None:
     if isfile(temporary_path):
         os.remove(temporary_path)
 
@@ -76,12 +75,10 @@ def log_files() -> Any:
     3. restore the original log file after the test
     """
 
-    LOG_FILE = join(
-        PROJECT_DIR, "logs", "archive", datetime.now().strftime("%Y-%m-%d.log")
-    )
-    TMP_LOG_FILE = join(
-        PROJECT_DIR, "logs", "archive", datetime.now().strftime("%Y-%m-%d.tmp.log")
-    )
+    LOG_FILE = join(PROJECT_DIR, "logs", "archive",
+                    datetime.now().strftime("%Y-%m-%d.log"))
+    TMP_LOG_FILE = join(PROJECT_DIR, "logs", "archive",
+                        datetime.now().strftime("%Y-%m-%d.tmp.log"))
 
     _save_file(LOG_FILE, TMP_LOG_FILE, "")
 
@@ -94,108 +91,3 @@ def log_files() -> Any:
     print("*** LOGS AFTER TEST:")
     print(logs_after_test, end="")
     print("***")
-
-
-"""
-@pytest.fixture(scope="function")
-def empty_active_mqtt_queue() -> Any:
-    start and stop the background processing of messages
-
-    active_mqtt_queue = utils.ActiveMQTTQueue()
-    active_mqtt_queue.remove_messages_by_status("pending")
-    active_mqtt_queue.remove_messages_by_status("in-progress")
-    active_mqtt_queue.remove_messages_by_status("done")
-
-    yield
-
-"""
-
-
-@pytest.fixture(scope="function")
-def mqtt_client_environment() -> Any:
-    """load the environment variables from config/.env.testing
-    and generate a dummy base-topic path"""
-
-    dotenv.load_dotenv(join(PROJECT_DIR, "config", ".env"))
-    dotenv.load_dotenv(join(PROJECT_DIR, "config", ".env.testing"))
-
-    timestamp = round(time.time())
-    os.environ["THINGSBOARD_MQTT_IDENTIFIER"] = "".join(
-        random.choices([chr(a) for a in range(ord("a"), ord("z") + 1)], k=20)
-    )
-    os.environ["THINGSBOARD_MQTT_BASE_TOPIC"] = f"development/test-{timestamp}/"
-
-    yield
-
-
-@pytest.fixture(scope="function")
-def mqtt_data_files() -> Any:
-    """start and stop the background sending loop of the SendingMQTTClient"""
-    print("clearing mqtt data files")
-
-    ACTIVE_MESSAGES_FILE = join(PROJECT_DIR, "data", "active-mqtt-messages.db")
-    TMP_ACTIVE_MESSAGES_FILE = join(PROJECT_DIR, "data", "active-mqtt-messages.tmp.db")
-
-    TEST_MESSAGE_DATE_STRING = datetime.now().strftime("%Y-%m-%d")
-    MESSAGE_ARCHIVE_FILE = join(
-        PROJECT_DIR,
-        "data",
-        "archive",
-        f"mqtt-messages-{TEST_MESSAGE_DATE_STRING}.json",
-    )
-    TMP_MESSAGE_ARCHIVE_FILE = join(
-        PROJECT_DIR,
-        "data",
-        "archive",
-        f"mqtt-messages-{TEST_MESSAGE_DATE_STRING}.tmp.json",
-    )
-
-    _save_file(ACTIVE_MESSAGES_FILE, TMP_ACTIVE_MESSAGES_FILE, None)
-    _save_file(MESSAGE_ARCHIVE_FILE, TMP_MESSAGE_ARCHIVE_FILE, None)
-
-    yield
-
-    _restore_file(ACTIVE_MESSAGES_FILE, TMP_ACTIVE_MESSAGES_FILE)
-    _restore_file(MESSAGE_ARCHIVE_FILE, TMP_MESSAGE_ARCHIVE_FILE)
-
-
-@pytest.fixture(scope="function")
-def messaging_agent_with_sending(
-    log_files: None,
-    mqtt_client_environment: None,
-    mqtt_data_files: None,
-    sample_config: None,
-) -> Any:
-    """start and stop the background processing of messages"""
-
-    config = utils.ConfigInterface.read()
-    config.active_components.send_messages_over_mqtt = True
-
-    procedures.MQTTAgent.init(config)
-    procedures.MQTTAgent.check_errors()
-
-    yield
-
-    procedures.MQTTAgent.check_errors()
-    procedures.MQTTAgent.deinit()
-
-
-@pytest.fixture(scope="function")
-def messaging_agent_without_sending(
-    log_files: None,
-    mqtt_client_environment: None,
-    mqtt_data_files: None,
-    sample_config: None,
-) -> Any:
-    """start and stop the background processing of messages"""
-
-    config = utils.ConfigInterface.read()
-    config.active_components.send_messages_over_mqtt = False
-
-    procedures.MQTTAgent.init(config)
-    procedures.MQTTAgent.check_errors()
-
-    yield
-
-    procedures.MQTTAgent.check_errors()
-    procedures.MQTTAgent.deinit()
