40b046a43fe27b943d481791d622e2d55702391e
Patrick <patrick.aigner@tum.de>
2024-10-08 14:53:21 +0200

    test variation of logger test

--- software/tests/communication_interfaces/test_logger.py
+++ software/tests/communication_interfaces/test_logger.py
@@ -31,9 +31,7 @@ def test_very_long_exception_cutting(log_files: None) -> None:
         "--- details: -----------------\n" + f"{details}\n" +
         "------------------------------\n")
 
-    expected_mqtt_message = (
-        f"pytests - {message[: (256 - 31)]} ... CUT (310 -> 256)" + " " +
-        f"{details[: (16384 - 25)]} ... CUT (20249 -> 16384)")
+    logger.error(message=message, details=details)
 
     expect_log_file_contents(
         forbidden_content_blocks=[expected_log_file_content])
