1d3e35c3118b5659c83a8321c0e6ca84a346e8fb
Patrick <patrick.aigner@tum.de>
2024-10-09 14:19:55 +0200

    redo poetry dependencies and update to latest versions

--- software/src/custom_types/config.py
+++ software/src/custom_types/config.py
@@ -1,49 +1,46 @@
 from __future__ import annotations
-
-from typing import Literal, Optional
-
-import pydantic
+from typing import Literal
+from pydantic import BaseModel, ConfigDict, Field
 
 
-class ActiveComponentsConfig(pydantic.BaseModel):
+class ActiveComponentsConfig(BaseModel):
+    model_config = ConfigDict(extra='forbid')
+
     run_calibration_procedures: bool
     send_messages_over_mqtt: bool
     run_hardware_tests: bool
 
-    class Config:
-        extra = "forbid"
-
 
 # -----------------------------------------------------------------------------
 
 
-class CalibrationGasConfig(pydantic.BaseModel):
+class CalibrationGasConfig(BaseModel):
+    model_config = ConfigDict(extra='forbid')
+
     valve_number: Literal[1, 2, 3, 4]
     bottle_id: str
 
-    class Config:
-        extra = "forbid"
 
+class CalibrationConfig(BaseModel):
+    model_config = ConfigDict(extra='forbid')
 
-class CalibrationConfig(pydantic.BaseModel):
-    average_air_inlet_measurements: int = pydantic.Field(..., ge=1)
-    calibration_frequency_days: int = pydantic.Field(..., ge=1)
-    calibration_hour_of_day: int = pydantic.Field(..., ge=0, le=23)
-    gas_cylinders: list[CalibrationGasConfig] = pydantic.Field(
-        ..., min_items=1, max_items=3
-    )
-    sampling_per_cylinder_seconds: int = pydantic.Field(..., ge=6, le=1800)
-    system_flushing_pump_pwm_duty_cycle: float = pydantic.Field(ge=0, le=1)
-    system_flushing_seconds: int = pydantic.Field(..., ge=0, le=600)
-
-    class Config:
-        extra = "forbid"
+    average_air_inlet_measurements: int = Field(..., ge=1)
+    calibration_frequency_days: int = Field(..., ge=1)
+    calibration_hour_of_day: int = Field(..., ge=0, le=23)
+    gas_cylinders: list[CalibrationGasConfig] = Field(...,
+                                                      min_length=1,
+                                                      max_length=3)
+    sampling_per_cylinder_seconds: int = Field(..., ge=6, le=1800)
+    system_flushing_pump_pwm_duty_cycle: float = Field(ge=0, le=1)
+    system_flushing_seconds: int = Field(..., ge=0, le=600)
 
 
 # -----------------------------------------------------------------------------
 
 
-class DocumentationConfig(pydantic.BaseModel):
+class DocumentationConfig(BaseModel):
+    model_config = ConfigDict(extra='forbid')
+
     site_name: str
     site_short_name: str
     site_observation_since: str
@@ -52,56 +49,47 @@ class DocumentationConfig(pydantic.BaseModel):
     maintenance_comment: str
     gmp343_sensor_id: str
 
-    class Config:
-        extra = "forbid"
-
 
 # -----------------------------------------------------------------------------
 
 
-class HardwareConfig(pydantic.BaseModel):
-    pump_pwm_duty_cycle: float = pydantic.Field(ge=0, le=1)
+class HardwareConfig(BaseModel):
+    model_config = ConfigDict(extra='forbid')
+
+    pump_pwm_duty_cycle: float = Field(ge=0, le=1)
     gmp343_optics_heating: bool
     gmp343_linearisation: bool
     gmp343_temperature_compensation: bool
     gmp343_relative_humidity_compensation: bool
     gmp343_pressure_compensation: bool
     gmp343_oxygen_compensation: bool
-    gmp343_filter_seconds_averaging: int = pydantic.Field(..., ge=0, le=60)
-    gmp343_filter_smoothing_factor: int = pydantic.Field(..., ge=0, le=255)
-    gmp343_filter_median_measurements: int = pydantic.Field(..., ge=0, le=13)
-
-    class Config:
-        extra = "forbid"
+    gmp343_filter_seconds_averaging: int = Field(..., ge=0, le=60)
+    gmp343_filter_smoothing_factor: int = Field(..., ge=0, le=255)
+    gmp343_filter_median_measurements: int = Field(..., ge=0, le=13)
 
 
 # -----------------------------------------------------------------------------
 
 
-class MeasurementConfig(pydantic.BaseModel):
+class MeasurementConfig(BaseModel):
+    model_config = ConfigDict(extra='forbid')
+
     average_air_inlet_measurements: int
-    procedure_seconds: int = pydantic.Field(..., ge=10, le=7200)
+    procedure_seconds: int = Field(..., ge=10, le=7200)
     valve_number: Literal[1, 2, 3, 4]
 
-    class Config:
-        extra = "forbid"
-
 
 # -----------------------------------------------------------------------------
 
 
-class Config(pydantic.BaseModel):
+class Config(BaseModel):
     """The config.json for each sensor"""
+    model_config = ConfigDict(extra='forbid')
 
-    revision: Optional[int]
-    version: str = pydantic.Field(
-        regex=r"^\d+\.\d+\.\d+(?:-(?:alpha|beta)\.\d+)?$"
-    )  # e.g., "1.2.3" or "99.0.1" or "42.1.0-alpha.6"
+    version: str = Field(pattern=r"^\d+\.\d+\.\d+(?:-(?:alpha|beta)\.\d+)?$"
+                         )  # e.g., "1.2.3" or "99.0.1" or "42.1.0-alpha.6"
     active_components: ActiveComponentsConfig
     calibration: CalibrationConfig
     documentation: DocumentationConfig
     hardware: HardwareConfig
     measurement: MeasurementConfig
-
-    class Config:
-        extra = "forbid"
