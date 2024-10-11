025deae8d67a975002fe9c92871de51ea6a855f4
Lars Frölich <lars.froelich@gmail.com>
2024-10-11 12:09:33 +0200

    updated docker run scripts (added privileged container permissions)

--- software/docker/docker_run.sh
+++ software/docker/docker_run.sh
@@ -8,7 +8,10 @@ export ACROPOLIS_DEPLOYMENT_ROOT_PATH=/root/deployment
 # Run the docker image
 docker run -it --rm --name ACROPOLIS_edge_$ACROPOLIS_MQTT_IDENTIFIER \
   --env-file .env \
-  -e ACROPOLIS_MQTT_IDENTIFIER="$ACROPOLIS_MQTT_IDENTIFIER" \
   -e ACROPOLIS_DEPLOYMENT_ROOT_PATH="$ACROPOLIS_DEPLOYMENT_ROOT_PATH" \
   -v "$GLOBAL_ACROPOLIS_DEPLOYMENT_ROOT_PATH":"$ACROPOLIS_DEPLOYMENT_ROOT_PATH" \
+  -v "/bin/vcgencmd":"/bin/vcgencmd" \
+  -v "/bin/uptime":"/bin/uptime" \
+  --network=host \
+  --privileged \
   tum-esm/acropolis/edge-node
--- software/docker/docker_run_daemon.sh
+++ software/docker/docker_run_daemon.sh
@@ -7,8 +7,11 @@ ACROPOLIS_DEPLOYMENT_ROOT_PATH=/root/deployment
 
 # Run the docker image
 docker run -d --restart unless-stopped --name ACROPOLIS_edge_$ACROPOLIS_MQTT_IDENTIFIER \
-  -e ACROPOLIS_MQTT_IDENTIFIER="$ACROPOLIS_MQTT_IDENTIFIER" \
   -e ACROPOLIS_DEPLOYMENT_ROOT_PATH="$ACROPOLIS_DEPLOYMENT_ROOT_PATH" \
   --env-file .env \
   -v "$GLOBAL_ACROPOLIS_DEPLOYMENT_ROOT_PATH":"$ACROPOLIS_DEPLOYMENT_ROOT_PATH" \
+  -v "/bin/vcgencmd":"/bin/vcgencmd" \
+  -v "/bin/uptime":"/bin/uptime" \
+  --network=host \
+  --privileged \
   tum-esm/acropolis/edge-node
