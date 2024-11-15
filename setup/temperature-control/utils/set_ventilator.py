ea352087ad2983680f2bc533c309e2c16c20ecd1
Patrick <patrick.aigner@tum.de>
2024-11-15 10:11:33 +0100

    improve stability

--- setup/temperature-control/utils/set_ventilator.py
+++ setup/temperature-control/utils/set_ventilator.py
@@ -2,13 +2,15 @@ import gpiozero
 from .get_gpio_pin_factory import get_gpio_pin_factory
 
 
-# initialize interface
-pin_factory = get_gpio_pin_factory()
-pin = gpiozero.OutputDevice(26, pin_factory=pin_factory)
+class VentilationControl():
 
-def set_venitlation_on():
-    pin.on()
-    
-def set_venitlation_off():
-    pin.off()
-    
+    def __init__(self):
+        # initialize interface
+        self.pin = gpiozero.OutputDevice(26,
+                                         pin_factory=get_gpio_pin_factory())
+
+    def set_venitlation_on(self):
+        self.pin.on()
+
+    def set_venitlation_off(self):
+        self.pin.off()
