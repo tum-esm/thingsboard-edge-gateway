b6742b5d31d13a12d2d7397d624f8a67fb0f1b81
Lars Frölich <lars.froelich@tum.de>
2024-11-17 20:30:24 +0100

    added ACROPOLIS_LIVE_DB_PATH environment variable

--- software/src/utils/message_queue.py
+++ software/src/utils/message_queue.py
@@ -7,7 +7,7 @@ from typing import Union
 from src import custom_types
 
 PROJECT_DIR = dirname(dirname(dirname(os.path.abspath(__file__))))
-ACTIVE_QUEUE_FILE = os.path.join(PROJECT_DIR, "data", "SQLite-Live-Queue.db")
+ACROPOLIS_LIVE_DB_PATH = os.environ.get("ACROPOLIS_LIVE_DB_PATH") or os.path.join(PROJECT_DIR, "data", "acropolis_live_db.db")
 
 THINGSBOARD_PAYLOADS = Union[custom_types.MQTTCO2Data,
                              custom_types.MQTTCO2CalibrationData,
@@ -18,10 +18,10 @@ THINGSBOARD_PAYLOADS = Union[custom_types.MQTTCO2Data,
 
 
 class MessageQueue:
+    """Uses an SQLite database to store messages to be forwarded to the ThingsBoard server by the gateway"""
 
     def __init__(self) -> None:
-        self.con = sqlite3.connect(ACTIVE_QUEUE_FILE)
-
+        self.con = sqlite3.connect(ACROPOLIS_LIVE_DB_PATH)
         self.con.execute("""
                 CREATE TABLE IF NOT EXISTS queue_out (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -32,13 +32,10 @@ class MessageQueue:
 
     def enqueue_message(self, timestamp: int,
                         payload: THINGSBOARD_PAYLOADS) -> None:
-
         new_message = {
             "ts": timestamp,
             "values": dataclasses.asdict(payload),
         }
-
         with self.con:
             sql_statement: str = "INSERT INTO queue_out (type, message) VALUES(?, ?)"
-            self.con.executemany(sql_statement,
-                                 [("MQTT_message", str(new_message))])
+            self.con.executemany(sql_statement,[("MQTT_message", str(new_message))])
