3f10c6dd23f4a6691c240c41e85f5641178c9fe7
Lars Frölich <lars.froelich@tum.de>
2024-11-17 22:39:10 +0100

    docker_build.sh: change image name to tum-esm/acropolis/edge-node

--- software/docker/docker_build.sh
+++ software/docker/docker_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Build the docker image
-docker build -t tum-esm/acropolis/sensor -f $(dirname "$0")/Dockerfile $(dirname "$0")/../
+docker build -t tum-esm/acropolis/edge-node -f $(dirname "$0")/Dockerfile $(dirname "$0")/../
