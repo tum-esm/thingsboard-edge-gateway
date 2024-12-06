d5935be7eb88f4c93d30c437b095f5366be80425
Patrick <patrick.aigner@tum.de>
2024-12-06 21:32:56 +0100

    remove deleted serial interface imports

--- software/controller/src/interfaces/__init__.py
+++ software/controller/src/interfaces/__init__.py
@@ -1,4 +1,4 @@
-from .serial_interfaces import SerialCO2SensorInterface, SerialI2CInterface, SerialOneDirectionalInterface
+from .serial_interfaces import SerialCO2SensorInterface
 from .state_interface import StateInterface
 from .hardware_interface import HardwareInterface
 from .config_interface import ConfigInterface
