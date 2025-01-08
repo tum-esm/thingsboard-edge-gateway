d018550db76589c9c6c701f475fb2b4111e12f29
Patrick <patrick.aigner@tum.de>
2025-01-08 12:17:52 +0100

    rename to ring_buffer

--- software/controller/src/utils/__init__.py
+++ software/controller/src/utils/__init__.py
@@ -4,6 +4,6 @@ from .gpio_pin_factory import get_gpio_pin_factory
 from .list_operations import avg_list
 from .logger import Logger
 from .message_queue import MessageQueue
-from .moving_average_queue import RingBuffer
+from .ring_buffer import RingBuffer
 from .shell_commands import run_shell_command, CommandLineException
 from .system_info import get_cpu_temperature, read_os_uptime
