1e053ec3a2adddfc5eab2bcbd91740582d57ad99
Patrick <patrick.aigner@tum.de>
2025-01-10 15:37:16 +0100

    setup documentation first version

---
+++ setup/edge-RPi-client/run_dockerized_gateway.sh
@@ -0,0 +1,14 @@
+docker container stop acropolis_edge_gateway
+docker container prune -f
+docker run -d \
+ --restart unless-stopped \
+ --privileged \
+ -v /:/host \
+ --name acropolis_edge_gateway \
+ --env ACROPOLIS_GATEWAY_DIR=/home/pi/acropolis/acropolis-edge/software/gateway \
+ --env ACROPOLIS_DATA_PATH=/home/pi/acropolis/data \
+ --env ACROPOLIS_GATEWAY_GIT_PATH=/home/pi/acropolis/acropolis-edge/.git \
+ --env ACROPOLIS_CONTROLLER_LOGS_PATH=/home/pi/acropolis/logs \
+ --env THINGSBOARD_PROVISION_DEVICE_KEY=... \
+ --env THINGSBOARD_PROVISION_DEVICE_SECRET=... \
+ acropolis-gateway-runner:latest --tb-host thingsboard.esm.ei.tum.de --tb-port 8843
