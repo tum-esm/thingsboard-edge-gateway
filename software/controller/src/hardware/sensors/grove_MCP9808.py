de8d1c02ca056504bec8013ca5d87960279ff640
Patrick <patrick.aigner@tum.de>
2025-01-13 18:07:59 +0100

    fix mypy issues

--- software/controller/src/hardware/sensors/grove_MCP9808.py
+++ software/controller/src/hardware/sensors/grove_MCP9808.py
@@ -4,26 +4,27 @@ except ImportError:
     pass
 
 from hardware.sensors.base_sensor import Sensor
+from custom_types import config_types
 
 
 class GroveMCP9808(Sensor):
     """Class for the Grove MCP9808 sensor."""
 
-    def __init__(self, config, simulate=False):
+    def __init__(self, config: config_types.Config, simulate: bool = False):
         super().__init__(config=config, simulate=simulate)
 
-    def _initialize_sensor(self):
+    def _initialize_sensor(self) -> None:
         """Initialize the sensor."""
         self.bus = smbus2.SMBus(1)
         self.adress = 0x18
         self.register = 0x05
 
-    def _shutdown_sensor(self):
+    def _shutdown_sensor(self) -> None:
         """Shutdown the sensor."""
         if self.bus:
             self.bus.close()
 
-    def _read(self):
+    def _read(self) -> float:
         """Read the sensor value."""
 
         result = self.bus.read_word_data(self.adress, self.register)
