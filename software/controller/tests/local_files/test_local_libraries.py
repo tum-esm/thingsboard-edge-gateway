68484553dd7e896fa4b8029a3cd23c77d70b4c18
Patrick <patrick.aigner@tum.de>
2025-01-16 14:15:08 +0100

    remove tests for old implementation

--- software/controller/tests/local_files/test_local_libraries.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-@pytest.mark.remote_update
-def test_local_libraries() -> None:
-    """checks whether the raspberry pi specific libraries can be loaded"""
-
-    import board
-    import RPi.GPIO
