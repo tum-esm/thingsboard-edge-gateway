b07fd021e96effdecd49f0d4ad3563be2cbdf7ac
Lars Frölich <lars.froelich@tum.de>
2025-01-31 13:27:39 +0100

    gateway: implemented help-command for rpc requests

--- software/gateway/src/on_mqtt_msg/on_rpc_request.py
+++ software/gateway/src/on_mqtt_msg/on_rpc_request.py
@@ -1,35 +1,75 @@
+import json
 import os
 import signal
+from time import sleep
 from typing import Any
 
 from modules.mqtt import GatewayMqttClient
+
+def rpc_reboot(rpc_msg_id: str, _method: Any, _params: Any):
+    """Reboot the device"""
+    print("[RPC] Rebooting...")
+    send_rpc_response(rpc_msg_id, "OK - Rebooting")
+    sleep(3)
+    os.system("reboot")
+
+def rpc_shutdown(rpc_msg_id: str, _method: Any, _params: Any):
+    print("[RPC] Shutting down...")
+    send_rpc_response(rpc_msg_id, "OK - Shutting down")
+    sleep(3)
+    os.system("shutdown now")
+
+def rpc_exit(rpc_msg_id: str, _method: Any, _params: Any):
+    print("[RPC] Exiting...")
+    send_rpc_response(rpc_msg_id, "OK - Exiting")
+    sleep(3)
+    signal.raise_signal(signal.SIGTERM)
+
+def rpc_ping(rpc_msg_id: str, _method: Any, _params: Any):
+    print("[RPC] Pong")
+    send_rpc_response(rpc_msg_id, "Pong")
+
+
+RPC_METHODS={
+    "reboot": {
+        "description": "Reboot the device",
+        "exec": rpc_reboot
+    },
+    "shutdown": {
+        "description": "Shutdown the device",
+        "exec": rpc_shutdown
+    },
+    "exit": {
+        "description": "Exits the gateway process (triggers gateway restart)",
+        "exec": rpc_exit
+    },
+    "ping": {
+        "description": "Ping the device (returns 'pong' reply)",
+        "exec": rpc_ping
+    }
+}
 
 
 def on_rpc_request(rpc_msg_id: str, method: Any, params: Any) -> None:
     """Handle incoming RPC requests"""
     print(f"RPC request: {rpc_msg_id} {method} ({params})")
     GatewayMqttClient().publish_log("INFO", f"RPC request: {method} ({params})")
-    if method == "reboot":
-        print("[RPC] Rebooting...")
-        send_rpc_response(rpc_msg_id, "OK - Rebooting")
-        os.system("reboot")
-    elif method == "shutdown":
-        print("[RPC] Shutting down...")
-        send_rpc_response(rpc_msg_id, "OK - Shutting down")
-        os.system("shutdown now")
-    elif method == "exit":
-        print("[RPC] Exiting...")
-        send_rpc_response(rpc_msg_id, "OK - Exiting")
-        signal.raise_signal(signal.SIGTERM)
+    if method in RPC_METHODS:
+        RPC_METHODS[method]["exec"](rpc_msg_id, method, params)
+    elif method == "list":
+        help_text = ["Available RPC methods:"]
+        for method_name, method_data in RPC_METHODS.items():
+            help_text.append(f"{method_name}: {method_data['description']}")
+        send_rpc_response(rpc_msg_id, help_text)
     else:
         print(f"Unknown RPC method: {method}")
         GatewayMqttClient().publish_log("ERROR", f"Unknown RPC method: {method}")
-        send_rpc_response(rpc_msg_id, "UNKNOWN_METHOD")
+        send_rpc_response(rpc_msg_id, f"Unknown RPC method: '{method}' - use command 'list' to get a list of available methods")
 
 
-def send_rpc_response(rpc_msg_id: str, response: str) -> bool:
+def send_rpc_response(rpc_msg_id: str, response: Any) -> bool:
     """Send an RPC response"""
     return GatewayMqttClient().publish_message_raw(
         "v1/devices/me/rpc/response/" + rpc_msg_id,
-        "{\"message\": \"" + response + "\"}"
+        json.dumps({"message": response})
     )
