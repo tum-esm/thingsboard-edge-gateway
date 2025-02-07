1b0a864cad5f2394411d8ba6252c50fede8713cd
Lars Frölich <lars.froelich@tum.de>
2025-02-07 11:11:28 +0100

    gateway logging: load module at runtime to avoid circular imports

--- software/gateway/src/modules/logging.py
+++ software/gateway/src/modules/logging.py
@@ -1,12 +1,16 @@
+import importlib
 import os
-
-
-from modules.mqtt import GatewayMqttClient
 
 # get log level from env var
 LOG_LEVEL = os.getenv('LOG_LEVEL') or 'INFO'
 
+# import GatewayMqttClient at runtime to avoid circular imports
+GatewayMqttClient = None
+
 def log(level, message):
+    global GatewayMqttClient
+    if GatewayMqttClient is None:
+        GatewayMqttClient = importlib.import_module('modules.mqtt').GatewayMqttClient
     print(f'[{level}] {message}')
     if level == 'DEBUG' and LOG_LEVEL != 'DEBUG':
         return
