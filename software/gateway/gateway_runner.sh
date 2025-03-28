26dd2c06d83dd97345ff109d96b94f728b22a1a6
Lars Frölich <lars.froelich@tum.de>
2025-03-28 10:14:51 +0100

    gateway_runner.sh: added verbose package caching for offline operations

--- software/gateway/gateway_runner.sh
+++ software/gateway/gateway_runner.sh
@@ -14,7 +14,13 @@ python3.12 -m venv --copies .venv
 source .venv/bin/activate
 
 echo "Installing dependencies in the virtual environment"
-pip install -r requirements.txt
+if ! pip install --find-links="~/.python-cache" --cache-dir="~/.python-cache" --no-index -r requirements.txt; then
+    echo "Failed to install dependencies from local cache, downloading and installing from PyPI"
+    pip download --cache-dir="~/.python-cache" -d "~/.python-cache" -r requirements.txt
+    pip install --find-links="~/.python-cache" --cache-dir="~/.python-cache" --no-index -r requirements.txt
+else
+    echo "Dependencies installed from local cache"
+fi
 
 echo "Running the gateway"
 echo "Args: $args"
