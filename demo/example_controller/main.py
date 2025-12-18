9d9589fe6c6c2d2843912bb772d36748c357855f
Lars Frölich <lars.froelich@gmail.com>
2025-12-18 16:21:16 +0100

    docker-demo: small improvements

--- demo/example_controller/main.py
+++ demo/example_controller/main.py
@@ -8,13 +8,15 @@ TEG_DATA_PATH = env_vars.TEG_DATA_PATH
 
 print(f"Starting controller with TEG_DATA_PATH={TEG_DATA_PATH}")
 
-print("Connecting to communication db...")
-db_connection = setup_and_connect_db(os.path.join(TEG_DATA_PATH, "communication_queue.db"))
+comm_db_path = os.path.join(TEG_DATA_PATH, "communication_queue.db")
+print(f"Connecting to communication db at '{comm_db_path}'...")
+db_connection = setup_and_connect_db(comm_db_path)
 
 print("Entering main loop...")
 # main loop
 while True:
     time.sleep(1)
 
+    print("Sending heartbeat to ThingsBoard")
     # write heartbeat to communication db
     write_health_check_message(db_connection, int(time.time() * 1000))
